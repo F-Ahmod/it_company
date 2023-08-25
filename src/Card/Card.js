3a5ff7682bd689defa6785e7846ea03bc74101e9
fatehahmod <ahmod.fateh@gmail.com>
2023-08-24 22:21:03 -0400

    added

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+//hello
 const Card = () => {
     return (
         <div className="card-group">
@@ -11,6 +11,7 @@ const Card = () => {
       <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
+  
   <div className="card">
     <img src="..." className="card-img-top" alt="..."/>
     <div className="card-body">
@@ -18,12 +19,15 @@ const Card = () => {
       <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
       <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
     </div>
+    
   </div>
+
   <div className="card">
     <img src="..." className="card-img-top" alt="..."/>
     <div className="card-body">
       <h5 className="card-title">Card title</h5>
       <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+
       <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
